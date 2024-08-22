16dda99eefd825ab398f2a7722b52a039b1261a3
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-08-22 15:10:06 +0800

    create base function for qr scanner

--- privy-mini-project/privy-mini-project/Screen/QRScanner/QRScannerView.swift
+++ privy-mini-project/privy-mini-project/Screen/QRScanner/QRScannerView.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class QRScannerView: UIViewController {
+class QRScannerView: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var captureSession: AVCaptureSession?
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -21,7 +26,95 @@ class QRScannerView: UIViewController {
         setupView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if captureSession?.isRunning == false {
+            startSession()
+        }
+    }
+    
+    private func startSession() {
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            self?.captureSession?.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if captureSession?.isRunning == true {
+            captureSession?.stopRunning()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        if captureSession?.isRunning == true {
+            captureSession?.stopRunning()
+        }
+    }
+    
     private func setupView() {
         view.backgroundColor = .white
+        scanner()
+    }
+    
+    private func scanner() {
+        captureSession = AVCaptureSession()
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        do {
+            let videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+            
+            if captureSession?.canAddInput(videoInput) == true {
+                captureSession?.addInput(videoInput)
+            } else {
+                failed()
+                return
+            }
+            
+            let metadataOutput = AVCaptureMetadataOutput()
+            
+            if captureSession?.canAddOutput(metadataOutput) == true {
+                captureSession?.addOutput(metadataOutput)
+                
+                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                metadataOutput.metadataObjectTypes = [.qr]
+            } else {
+                failed()
+                return
+            }
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = .resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            startSession()
+        } catch {
+            failed()
+            return
+        }
+    }
+    
+    func failed() {
+        let alertController = UIAlertController(title: "Not supported", message: "Your device does not support QR code scanning.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
+        captureSession = nil
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession?.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            
+            print("QR Code Detected: \(stringValue)")
+        }
     }
 }
