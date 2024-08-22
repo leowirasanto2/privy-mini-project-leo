28264a5d8452044e37824f665fb1257c2a33af68
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-08-22 12:23:15 +0800

    initiate project

---
+++ privy-mini-project/privy-mini-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  privy-mini-project
+//
+//  Created by Leo Wirasanto Laia on 22/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
